c0bb1bf9f55901400e1bd2e7872b6f53d990b586
dave glynn <dave@ocl.ie>
2016-05-31 15:36:34 +0100

    change pasword len validator

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -57,7 +57,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
-                len: [7, 100]
+                len: [6, 100]
             },
             set: function(value) {
                 var salt = bcrypt.genSaltSync(10);
