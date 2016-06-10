187b20c65b901218c1c2a06546299e092a9f1eae
dave glynn <dave@ocl.ie>
2016-06-10 19:46:49 +0100

    cors

--- app.js
+++ app.js
@@ -12,11 +12,18 @@ var PORT = process.env.PORT || 3000;
 
 // allow cross browser
 app.use(function (req, res, next) {
-     res.header('Access-Control-Allow-Origin', '*');
-     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,X-Auth-Token , Authorization');
-     res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS');
-     next();
+
+     if ('OPTIONS' == req.method) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,X-Auth-Token , Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS');
+        res.send(200);
+    }
+    else {
+      next();
+    }
 });
+ 
 
 // middleware to console log every request
 app.use(function(req, res, next) {
