2f2dc091c19124668f364d1981552c91e3b28808
dave glynn <dave@ocl.ie>
2016-06-10 14:45:41 +0100

    cors

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ var PORT = process.env.PORT || 3000;
 app.use(function (req, res, next) {
     //res.header("Access-Control-Allow-Origin", "*");
     //res.header("Access-Control-Allow-Headers, "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    
+    //res.setHeader('Access-Control-Allow-Origin', '*'); 
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    //next();
+
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers",
+        'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
     next();
+
 });
 
 // middleware to console log every request
