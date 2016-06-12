b9b7c9a830f57ad40b79d8c1b39ba6c418fe64b9
dave glynn <dave@ocl.ie>
2016-06-12 17:46:13 +0100

    token auth

--- middleware.js
+++ middleware.js
@@ -10,19 +10,31 @@ var _ = require('underscore');
 module.exports = function(db) {
 
 	return {
-		    requireAuthentication: function(req, res, next) {
-			    var token = req.query.Auth || req.get('Auth') ||  '';
-			    db.token.findOne({
-				    where: { 
-					    tokenHash: cryptojs.MD5(token).toString()
-				    }
-			        }).then(function(tokenInstance) {
-				        if (!tokenInstance) {
-					        throw new Error();
-				        }
-
-				        req.token = tokenInstance;
-				        return db.user.findByToken(token);
+        requireAuthentication: function (req, res, next) {
+            
+            //var token = req.query.Auth || req.get('Auth') ||  '';            
+            // get token from header(for Postman testing) or as query parm from app client
+            var token = '';
+            if (req.query.Auth) {
+                var token = req.query.Auth;
+            } else if (req.get('Auth')) {
+                    var token = req.get('Auth');
+            }
+            else {
+                token = '';
+            }
+            
+			db.token.findOne({
+			 where: { 
+			  tokenHash: cryptojs.MD5(token).toString()
+			 }
+			    }).then(function(tokenInstance) {
+			     if (!tokenInstance) {
+			      throw new Error();
+			     }
+                
+                req.token = tokenInstance;
+				    return db.user.findByToken(token);
 			        }).then(function(user) {
                         req.user = user;
 				        next();
