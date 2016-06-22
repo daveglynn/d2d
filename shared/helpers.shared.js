94004d7a2b5263d63e0bd1d2597d601aadbfe495
dave glynn <dave@ocl.ie>
2016-06-22 08:52:49 +0100

    error refactor

---
+++ shared/helpers.shared.js
@@ -0,0 +1,21 @@
+﻿/******************************************************************************************************
+ helpers extension
+******************************************************************************************************/
+ 
+var env = process.env.NODE_ENV || 'development';
+
+module.exports = {
+
+ getFunctionName: function (name) {
+    
+        if (env === 'realproduction') {
+            return '';
+        } else {
+            return name;
+        }
+    }
+}
+
+
+
+ 
