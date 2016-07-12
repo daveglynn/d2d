55fd76fdfa93cb37b2ab9e6fb29f1e82837683f1
dave glynn <dave@ocl.ie>
2016-07-12 17:36:57 +0100

    error processing changes

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -34,7 +34,7 @@ module.exports.usersLogin = function(req, res) {
             user: userInstance.toPublicJSON()
         })
     }).catch(function() {
-        res.status(400).send();
+        res.status(400).json({message: 'Signin UnSuccessful'});
     });
 };
 
