98df6f1b5b3834a90ca25141a54d79342e4d869b
dave glynn <dave@ocl.ie>
2016-11-03 14:51:10 +0000

    add in recordDescription

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -84,8 +84,8 @@ module.exports.getUsersAll = function(req, res) {
 	where = common.setClauseTenantId(req, where); 
     var attributes = common.excludeAttributes();
 	 			
-	var include = [{ model: db.language,attributes: ['id','firstName','lastName']} 		
-				   ,{model: db.profile,attributes: ['id','firstName','lastName']}   ]; 	
+	var include = [{ model: db.language,attributes: ['id','name']} 		
+				   ,{model: db.profile,attributes: ['id','name']}   ]; 	
 	
     db.user.findAll({
         attributes: attributes,
