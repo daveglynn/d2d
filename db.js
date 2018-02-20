c8154e4b0fcc407c00759f2ab130f5519558da8e
dave glynn <dave@ocl.ie>
2018-02-20 12:12:29 +0000

    azure deploy

--- db.js
+++ db.js
@@ -22,7 +22,7 @@ var sequelize;
 	sequelize = new Sequelize('postgresqldatabase46947', 'postgresqldbuser', 'd2d-demo', {
 		host: 'd2d-demo-postgresqldbserver.postgres.database.azure.com',
 		port: 5432,
-		dialect: 'mysql',
+		dialect: 'postgres',
 		define: {
 					underscored: false,
 					freezeTableName: true,
